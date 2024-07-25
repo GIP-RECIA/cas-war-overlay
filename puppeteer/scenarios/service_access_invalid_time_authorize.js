c2fed4dfc96f0cc54d1ccef51ef4fa7c395cbc30
cailbourdin <nathan.cailbourdin@recia.fr>
2024-07-25 11:16:06 +0200

    test: tests puppeteer de restriction d'accès à un service sur une plage de temps donnée

---
+++ puppeteer/scenarios/service_access_invalid_time_authorize.js
@@ -0,0 +1,27 @@
+const cas = require("../cas.js");
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+
+    try {
+        const page = await browser.newPage();
+        const client = await page.createCDPSession();
+        const casHost = "https://localhost:8443";
+        const service = "http://localhost:8010/test"
+
+        // Login to cas
+        await cas.loginWith(page, casHost, service, "test2", "test")
+
+        // Assert that TGC exists
+        await cas.verifyTGC(client)
+
+        process.exit(0)
+
+    } catch (e) {
+        cas.loge(e);
+        process.exit(1)
+    } finally {
+        await browser.close();
+    }
+})();
---
+++ puppeteer/scenarios/service_access_valid_time_unauthorized.js
@@ -0,0 +1,27 @@
+const cas = require("../cas.js");
+const puppeteer = require('puppeteer');
+const assert = require("assert");
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+
+    try {
+        const page = await browser.newPage();
+        const casHost = "https://localhost:8443";
+        const service = "http://localhost:8009/test"
+
+        // Go to login page
+        const response = await page.goto(`${casHost}/cas/login?service=${service}`);
+
+        // It should respond with a 403 because we can't access the service
+        assert(response.status() == 403)
+
+        process.exit(0)
+
+    } catch (e) {
+        cas.loge(e);
+        process.exit(1)
+    } finally {
+        await browser.close();
+    }
+})();
