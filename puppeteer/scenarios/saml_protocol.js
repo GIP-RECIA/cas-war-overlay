6e1e7468fbbfea8352de05ad0147131483208c6c
cailbourdin <nathan.cailbourdin@recia.fr>
2024-09-10 10:24:34 +0200

    test: ajout scnéario puppeteer protocole SAML

---
+++ puppeteer/scenarios/saml_protocol.js
@@ -0,0 +1,36 @@
+const cas = require("../cas.js");
+const puppeteer = require('puppeteer');
+const assert = require("assert");
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+
+    try {
+        const page = await browser.newPage();
+        const client = await page.createCDPSession();
+
+        // Login to cas
+        await page.goto("http://localhost:8011/?sso");
+        await cas.typeCredentialsAndEnter(page, "test1", "test");
+        await page.waitForNavigation();
+
+        // Assert that TGC exists
+        await cas.verifyTGC(client)
+
+        // waitForNetworkIdle is necessary in that case to obtain pageContent, or we get an error "Execution context was destroyed"
+        await page.waitForNetworkIdle();
+        const pageContent = await page.content();
+
+        // Assert that user is logged in
+        assert(pageContent.includes("authenticationDate"))
+        assert(pageContent.includes("Logout"))
+        
+        process.exit(0)
+
+    } catch (e) {
+        cas.loge(e);
+        process.exit(1)
+    } finally {
+        await browser.close();
+    }
+})();
