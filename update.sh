2a66e53e9591d9717fe5bc07459cb0c9e930d177
cailbourdin <nathan.cailbourdin@recia.fr>
2024-11-29 17:19:36 +0100

    chore: maj update script nouveau fichier DelegatedClientAuthenticationRedirectAction

--- update.sh
+++ update.sh
@@ -22,6 +22,7 @@ cp src/main/java/org/apereo/cas/web/flow/resolver/impl/DefaultCasDelegatingWebfl
 cp src/main/java/org/apereo/cas/web/flow/BaseServiceAuthorizationCheckAction.java diff/
 cp src/main/java/org/apereo/cas/oidc/token/OidcIdTokenGeneratorService.java diff/
 cp src/main/java/org/apereo/cas/support/saml/idp/metadata/generator/BaseSamlIdPMetadataGenerator.java diff/
+cp src/main/java/org/apereo/cas/web/flow/actions/DelegatedClientAuthenticationRedirectAction.java diff/
 
 # Télécharger les fichiers de la nouvelle version et les placer dans le dossier 'diff'
 echo "Téléchargement des fichiers de la nouvelle version"
@@ -31,6 +32,7 @@ wget -q -O diff/DefaultCasDelegatingWebflowEventResolver_$VERSION.java https://r
 wget -q -O diff/BaseServiceAuthorizationCheckAction_$VERSION.java https://raw.githubusercontent.com/apereo/cas/refs/tags/v$VERSION/support/cas-server-support-actions-core/src/main/java/org/apereo/cas/web/flow/BaseServiceAuthorizationCheckAction.java
 wget -q -O diff/OidcIdTokenGeneratorService_$VERSION.java https://raw.githubusercontent.com/apereo/cas/refs/tags/v$VERSION/support/cas-server-support-oidc-core-api/src/main/java/org/apereo/cas/oidc/token/OidcIdTokenGeneratorService.java
 wget -q -O diff/BaseSamlIdPMetadataGenerator_$VERSION.java https://raw.githubusercontent.com/apereo/cas/refs/tags/v$VERSION/support/cas-server-support-saml-idp-core/src/main/java/org/apereo/cas/support/saml/idp/metadata/generator/BaseSamlIdPMetadataGenerator.java
+wget -q -O diff/DelegatedClientAuthenticationRedirectAction_$VERSION.java https://raw.githubusercontent.com/apereo/cas/refs/tags/v$VERSION/support/cas-server-support-pac4j-webflow/src/main/java/org/apereo/cas/web/flow/actions/DelegatedClientAuthenticationRedirectAction.java
 
 # Pour les fichiers de l'overlay c'est un peu différent il faut générer un nouvel overlay, l'extraire puis les recopier
 mkdir tmp
@@ -59,6 +61,7 @@ meld diff/DefaultCasDelegatingWebflowEventResolver.java diff/DefaultCasDelegatin
 meld diff/BaseServiceAuthorizationCheckAction.java diff/BaseServiceAuthorizationCheckAction_$VERSION.java
 meld diff/OidcIdTokenGeneratorService.java diff/OidcIdTokenGeneratorService_$VERSION.java
 meld diff/BaseSamlIdPMetadataGenerator.java diff/BaseSamlIdPMetadataGenerator_$VERSION.java
+meld diff/DelegatedClientAuthenticationRedirectAction.java diff/DelegatedClientAuthenticationRedirectAction_$VERSION.java
 
 # Une fois la comparaison finie remplacer les fichiers dans src/ par ceux modifiés dans diff/
 echo "Recopie des fichiers mis à jour dans src"
@@ -70,6 +73,7 @@ cp diff/DefaultCasDelegatingWebflowEventResolver.java src/main/java/org/apereo/c
 cp diff/BaseServiceAuthorizationCheckAction.java src/main/java/org/apereo/cas/web/flow/BaseServiceAuthorizationCheckAction.java
 cp diff/OidcIdTokenGeneratorService.java src/main/java/org/apereo/cas/oidc/token/OidcIdTokenGeneratorService.java
 cp diff/BaseSamlIdPMetadataGenerator.java src/main/java/org/apereo/cas/support/saml/idp/metadata/generator/BaseSamlIdPMetadataGenerator.java
+cp diff/DelegatedClientAuthenticationRedirectAction.java src/main/java/org/apereo/cas/web/flow/actions/DelegatedClientAuthenticationRedirectAction.java
 
 # Supprimer le dossier diff une fois qu'on a fini de faire les modifs
 echo "Suppression du dosser diff pour prochaine màj"
