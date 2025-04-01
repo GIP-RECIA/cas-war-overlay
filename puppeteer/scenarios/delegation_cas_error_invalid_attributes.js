cb41726e22c7c6e04e893177e137df9aaacb8eaa
cailbourdin <nathan.cailbourdin@recia.fr>
2025-04-01 16:08:28 +0200

    test: fix confusion entre auth local et delegation cas

--- puppeteer/scenarios/delegation_cas_error_invalid_attributes.js
+++ puppeteer/scenarios/delegation_cas_error_invalid_attributes.js
@@ -10,8 +10,15 @@ const assert = require("assert");
         const casHost = "https://localhost:8443";
         const service = "http://localhost:8030/test"
 
-        // Login to CAS
-        await cas.loginWith(page, casHost, service, "test2", "test")
+        // Goto CAS login page
+        await page.goto(`${casHost}/cas/login?service=${service}`);
+
+        // Click on external idp button
+        await page.click("#DELEGTEST");
+
+        // Enter credentials and validate
+        await cas.typeCredentialsAndEnter(page, "test2", "test");
+        await page.waitForNavigation();
 
         // Assert that the we got the right error message
         const pageContent = await page.content();
