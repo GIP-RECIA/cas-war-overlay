37d7f93b9a5fb48db8ace70fe6619fb80a223e71
cailbourdin <nathan.cailbourdin@recia.fr>
2025-04-04 12:55:06 +0200

    test: ajout test de health check

---
+++ puppeteer/scenarios/health_check.js
@@ -0,0 +1,24 @@
+const cas = require("../cas.js");
+const puppeteer = require('puppeteer');
+const assert = require("assert");
+
+(async () => {
+    const browser = await puppeteer.launch(cas.browserOptions());
+
+    try {
+        const page = await browser.newPage();
+        const casHost = "https://localhost:8443";
+
+        await page.goto(`${casHost}/cas/actuator/health`);
+        const pageContent = await page.content();
+        assert(pageContent.includes('"status":"UP"'))
+
+        process.exit(0)
+
+    } catch (e) {
+        cas.loge(e);
+        process.exit(1)
+    } finally {
+        await browser.close();
+    }
+})();
