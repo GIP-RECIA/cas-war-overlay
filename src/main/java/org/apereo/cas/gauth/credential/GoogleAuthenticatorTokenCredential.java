910ed9ffed869eb216f98574925349e450c50b08
cailbourdin <nathan.cailbourdin@recia.fr>
2025-05-27 15:53:22 +0200

    fix: changement du nom du paramètre token pour login otp device
    Changement de token pour otpToken pour ne pas avoir de collision avec le paramètre token du portail

---
+++ src/main/java/org/apereo/cas/gauth/credential/GoogleAuthenticatorTokenCredential.java
@@ -0,0 +1,41 @@
+package org.apereo.cas.gauth.credential;
+
+import org.apereo.cas.authentication.credential.OneTimeTokenCredential;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serial;
+
+/**
+ * This is {@link GoogleAuthenticatorTokenCredential}.
+ *
+ * @author Misagh Moayyed
+ * @since 5.0.0
+ */
+@NoArgsConstructor(force = true)
+@Getter
+@Setter
+@ToString(callSuper = true)
+public class GoogleAuthenticatorTokenCredential extends OneTimeTokenCredential {
+
+    @Serial
+    private static final long serialVersionUID = -7570600701132111037L;
+
+    private Long accountId;
+    // Customization : add new field to avoid collision with portal parameter
+    private String otpToken;
+
+    public GoogleAuthenticatorTokenCredential(final String token, final Long accountId) {
+        super(token);
+        setAccountId(accountId);
+    }
+    
+    // Field is only used in model to get the value, then we use the normal CAS parameter
+    public void setOtpToken(String token) {
+        super.setToken(token);
+    }
+
+}
