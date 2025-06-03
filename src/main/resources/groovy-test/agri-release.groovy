545bd114333a0d703ef08a7da1cb934b252d2872
cailbourdin <nathan.cailbourdin@recia.fr>
2025-06-03 14:28:55 +0200

    test: ajout test d'intégration compte inconnu en délégation classique

---
+++ src/main/resources/groovy-test/agri-release.groovy
@@ -0,0 +1,12 @@
+import java.util.*
+
+def run(final Object... args) {
+    def (currentAttributes,logger,principal,service) = args
+    logger.error("Current attributes received are {}", currentAttributes)
+    // Dans ce cas spécifique on veut retourner un mail qui n'existe pas dans le LDAP pour lever une exception
+    if(currentAttributes.get("mail")[0] == "test10.test@idp1.com"){
+        return [mail:["invalid.test@nodomain.com"]]
+    }
+    // Dans les autres cas on retourne un mail qui existe vraiment
+    return [mail:["test7.test@idp1.com"]]
+}
