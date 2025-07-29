247385b7aeffddb878fef47440abce2560050b5b
cailbourdin <nathan.cailbourdin@recia.fr>
2025-07-29 11:54:32 +0200

    feat: amélioration ui page d'interruption

--- src/main/java/org/apereo/cas/interrupt/DomainChangeInterruptInquirer.java
+++ src/main/java/org/apereo/cas/interrupt/DomainChangeInterruptInquirer.java
@@ -18,7 +18,6 @@ import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
