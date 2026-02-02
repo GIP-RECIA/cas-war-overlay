a7ef6fced99b5890e9a8383b1779658385997cf5
cailbourdin <nathan.cailbourdin@recia.fr>
2026-02-02 15:23:51 +0100

    test: vérification de la stratégie de merge des attributs du principal
    - Attributs récupérés depuis le LDAP en préférence et pas la source d'authentification

--- src/main/resources/groovy-test/agri-release.groovy
+++ src/main/resources/groovy-test/agri-release.groovy
@@ -5,11 +5,11 @@ def run(final Object... args) {
     logger.error("Current attributes received are {}", currentAttributes)
     // Dans ce cas spécifique on veut retourner un mail qui n'existe pas dans le LDAP pour lever une exception
     if(currentAttributes.get("mail")[0] == "test10.test@idp1.com"){
-        return [mail:["invalid.test@nodomain.com"]]
+        return [mail:["invalid.test@nodomain.com"], ENTPersonLogin:["valueThatShouldBeReplaced"]]
     }
     if(currentAttributes.get("mail")[0] == "test.test14@test.com"){
-        return [mail:["test.test14@test.com"]]
+        return [mail:["test.test14@test.com"], ENTPersonLogin:["valueThatShouldBeReplaced"]]
     }
     // Dans les autres cas on retourne un mail qui existe vraiment
-    return [mail:["test7.test@idp1.com"]]
+    return [mail:["test7.test@idp1.com"], ENTPersonLogin:["valueThatShouldBeReplaced"]]
 }
