061a1708a582fea273f8b757cb9f80d41a1f19f4
cailbourdin <nathan.cailbourdin@recia.fr>
2026-09-25 17:05:38 +0200

    test: fix scenario delegation without tst

--- puppeteer/scenarios/slo_delegation_without_tst.js
+++ puppeteer/scenarios/slo_delegation_without_tst.js
@@ -31,8 +31,8 @@ const assert = require("assert");
 
         // Delete the TST like if it was missing from the ticket registry
         const { cookies } = await client.send('Network.getAllCookies');
-        const cookieToDelete = cookies.find(c => c.name === 'SESSION');
-        assert(cookieToDelete.name.includes('SESSION'))
+        const cookieToDelete = cookies.find(c => c.name === 'CASTESTSESSION');
+        assert(cookieToDelete.name.includes('CASTESTSESSION'))
         if (cookieToDelete) {
             await client.send('Network.deleteCookies', {
             name: cookieToDelete.name,
